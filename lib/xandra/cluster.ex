0face3ac8e7ab5962ac6b161bd50582642e6bb1e
Jens Fischer <web@jens-fischer.eu>
2019-05-09 18:21:34 +0200

    Add protocol version to cluster default start options

--- lib/xandra/cluster.ex
+++ lib/xandra/cluster.ex
@@ -72,7 +72,8 @@ defmodule Xandra.Cluster do
 
   @default_start_options [
     nodes: ["127.0.0.1"],
-    idle_interval: 30_000
+    idle_interval: 30_000,
+    protocol_version: 3
   ]
 
   defstruct [
