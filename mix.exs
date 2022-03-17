128e255ccbce59bf1e83f7ed24851347588e0746
Jens Fischer <web@jens-fischer.eu>
2022-03-17 11:50:47 +0100

    Allow newer decimal version

--- mix.exs
+++ mix.exs
@@ -58,7 +58,7 @@ defmodule Xandra.Mixfile do
   defp deps() do
     [
       {:db_connection, "~> 2.0"},
-      {:decimal, "~> 1.7", optional: true},
+      {:decimal, "~> 1.7 or ~> 2.0", optional: true},
       {:nimble_options, "~> 0.4.0"},
 
       # Dev and test dependencies
